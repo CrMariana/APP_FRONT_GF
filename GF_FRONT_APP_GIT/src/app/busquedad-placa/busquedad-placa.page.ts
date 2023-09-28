12a6b8b195587e1af3daa23a4c031244aefa0a35
CrMariana <cristobalmariana2302@gmail.com>
2023-09-28 17:58:03 -0500

    busquedad-placa

--- GF_FRONT_APP_GIT/src/app/busquedad-placa/busquedad-placa.page.ts
+++ GF_FRONT_APP_GIT/src/app/busquedad-placa/busquedad-placa.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-busquedad-placa',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./busquedad-placa.page.scss'],
 })
 export class BusquedadPlacaPage implements OnInit {
+  yearInput: number | null = null; // Variable para almacenar el año ingresado
 
-  constructor() { }
+  constructor(private alertController: AlertController) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async buscarPorAnio() {
+    if (this.yearInput !== null) {
+      if (this.yearInput >= 1999 && this.yearInput <= 2017) {
+        // El año está dentro del rango permitido, muestra una alerta de búsqueda exitosa
+        const alert = await this.alertController.create({
+          header: 'Búsqueda exitosa',
+          message: `Búsqueda para el año ${this.yearInput}`,
+          buttons: ['OK']
+        });
+
+        await alert.present();
+      } else {
+        // El año está fuera del rango permitido, muestra una alerta de error de rango
+        const alert = await this.alertController.create({
+          header: 'Error',
+          message: 'El año debe estar entre 1999 y 2017',
+          buttons: ['OK']
+        });
+
+        await alert.present();
+      }
+    } else {
+      // El año ingresado es nulo o no es un número, muestra una alerta de error de formato
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Ingrese un año válido (formato: AAAA)',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+    }
   }
-
 }
